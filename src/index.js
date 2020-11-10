ef0240d05977bcf593fe438244e4e8e5632b861d
kickvire <varuit2020@gmail.com>
2020-11-10 23:29:32 +0530

    change

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
+    if(req.url==='/welcome')
+         {
+             res.setHeader("Content-type","text/plain");
+             res.statusCode=200;
+             res.end("Welcome to Dominos!")
+         }
+     if(req.url==='/contact')
+     {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({phone: '18602100000',
+        email: 'guestcaredominos@jublfood.com'
+        }))
+     }
   
 }
+server.listen(3000);
 
 module.exports = httpServer;
